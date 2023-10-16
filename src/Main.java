e2f8e54623123895d73158d5595974ab2e592544
Brian Alejandro Madsen <brianmadsen1992@hotmail.com>
2023-10-16 20:32:26 +0200

    Working on it got a bit stucked

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ import java.util.HashSet;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
 
-
-
+      System.out.println("Testing water "+ TrapRainingWater42.measureWater(7,10, TrapRainingWater42.test));
+        //System.out.println("Testing trap "+ TrapRainingWater42.trap(TrapRainingWater42.test));
     }
 
 
--- src/TrapRainingWater42.java
+++ src/TrapRainingWater42.java
@@ -1,6 +1,7 @@
 
 public class TrapRainingWater42 {
     public TrapRainingWater42() {
+
             /*
     * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
@@ -19,14 +20,55 @@ public class TrapRainingWater42 {
     * */
     }
 
+    public static int[] test = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
 
 
+    public static int trap(int[] height) {
+        int result = 0, leftPoint = 0, rightPointer = 0, higestPoint = 0, water = 0;
 
-    public int trap(int[] height) {
-        int result = 0 , l = 0 , r = 0, higestPoint= 0  ;
-        while(r < height.length){
+        //First iteration get all the water in between the hiegest poitns
+        while (rightPointer < height.length) {
+            if (height[rightPointer] > higestPoint) {
+                higestPoint = height[rightPointer];
+                water += measureWater(leftPoint, rightPointer, height);
+                leftPoint = rightPointer;
+            } else {
+                rightPointer++;
+            }
 
         }
-        return result;
+
+        //Second iteration to find the second
+
+
+
+        return water;
     }
+
+    public static int measureWater(int leftPoint, int rightPoint, int[] height) {
+        int water = 0;
+        for (int a = leftPoint; a < rightPoint; a++) {
+            if ((height[a] < height[leftPoint]) && (height[a] < height[rightPoint])) {
+                water += Math.min(height[leftPoint], height[rightPoint]) - (height[a]);
+            }
+        }
+
+        return water;
+    }
+
+    public static int measureWaterEnd(int leftPoint, int rightPoint, int[] height) {
+        int water = 0;
+         if ( height[rightPoint] < height[leftPoint]) {
+
+         }
+        for (int a = leftPoint; a < rightPoint; a++) {
+            if ((height[a] < height[leftPoint]) && (height[a] < height[rightPoint])) {
+                water += Math.min(height[leftPoint], height[rightPoint]) - (height[a]);
+            }
+        }
+
+        return water;
+    }
+
+
 }
